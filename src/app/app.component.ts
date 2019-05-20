74cd15045fe9c4696a3a6b2f0de87ffc42161e36
CorentinCrz <33249071+CorentinCrz@users.noreply.github.com>
2019-05-20 10:49:07 +0200

    first steps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,74 @@
-import { Component } from '@angular/core';
+/**
+ * Configuration
+ */
+//NodeJs
+import { Component, OnInit } from '@angular/core';
 
+// Declaration
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'ang-first';
+
+/**
+ * Export
+ */
+export class AppComponent implements OnInit {
+  /**
+   * Declaration
+   */
+  public title: string;
+  public dataCollection: Array<any>;
+
+  // class injection
+  constructor(){
+    this.title = 'Hello Angular';
+    this.dataCollection = [
+        {
+          "id": "1493",
+          "employee_name": "Julien",
+          "employee_salary": "0",
+          "employee_age": "39"
+        },
+        {
+          "id": "1494",
+          "employee_name": "AL",
+          "employee_salary": "2200",
+          "employee_age": "25"
+        },
+        {
+          "id": "1496",
+          "employee_name": "Eminem",
+          "employee_salary": "2323",
+          "employee_age": "34"
+        },
+        {
+          "id": "1497",
+          "employee_name": "Adam 5",
+          "employee_salary": "1111",
+          "employee_age": "35"
+        },
+        {
+          "id": "1500",
+          "employee_name": "Adam 8",
+          "employee_salary": "1111",
+          "employee_age": "35"
+        }
+      ];
+  }
+
+  /**
+  * Méthodes
+  */
+ public sayHello = ( name: string ) => {
+   this.title = `Hello ${name}`
+ }
+
+  /**
+   * Hooks
+   */
+  // eq. DOMContentLoadeed
+  ngOnInit(): void {
+    console.log('Component is fully loaded');
+  }
 }
